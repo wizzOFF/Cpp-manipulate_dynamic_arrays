6f9ee2f191764aa7cac3cab8994793df7942ccad
WizzOFF <maher_off@proton.me>
2025-03-29 20:29:30 +0100

    manipulate_dynamic_arrays.cpp: fill table directly

--- manipulate_dynamic_arrays.cpp
+++ manipulate_dynamic_arrays.cpp
@@ -7,5 +7,11 @@ int main()
 {
    vector<int> tableau(5);
 
+   vector<int> tableau(5, 3);  //Crée un tableau de 5 entiers valant tous 3
+   vector<string> listeNoms(12, "Sans nom");
+   //Crée un tableau de 12 strings valant toutes « Sans nom »
+
+   vector<double> tableauSansCases; //Crée un tableau de 0 nombre à virgule
+
    return 0;
 }
