ea95ed4d8b2f48995fade9eb152c6d5fc53b4e3c
WizzOFF <maher_off@proton.me>
2025-03-29 20:37:00 +0100

    manipulate_dynamic_arrays.cpp: access elements of a dynamic array

--- manipulate_dynamic_arrays.cpp
+++ manipulate_dynamic_arrays.cpp
@@ -5,13 +5,25 @@ using namespace std;
 
 int main()
 {
-   vector<int> tableau(5);
+   /*
+    vector<int> tableau(5);
 
    vector<int> tableau(5, 3);  //Crée un tableau de 5 entiers valant tous 3
    vector<string> listeNoms(12, "Sans nom");
    //Crée un tableau de 12 strings valant toutes « Sans nom »
 
    vector<double> tableauSansCases; //Crée un tableau de 0 nombre à virgule
+   */
+
+   int const nombreMeilleursScores(5);  //La taille du tableau
+   
+   vector<int> meilleursScores(nombreMeilleursScores);  //Déclaration du tableau
+   
+   meilleursScores[0] = 118218;  //Remplissage de la première case
+   meilleursScores[1] = 100432;  //Remplissage de la deuxième case
+   meilleursScores[2] = 87347;   //Remplissage de la troisième case
+   meilleursScores[3] = 64523;   //Remplissage de la quatrième case
+   meilleursScores[4] = 31415;   //Remplissage de la cinquième case
 
    return 0;
 }
