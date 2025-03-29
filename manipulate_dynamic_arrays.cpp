35bfcb16d789540a74e46fe8e5045f7f43561ee6
WizzOFF <maher_off@proton.me>
2025-03-29 20:52:07 +0100

    manipulate_dynamic_arrays.cpp: change the size of a dynamic table > add cell

--- manipulate_dynamic_arrays.cpp
+++ manipulate_dynamic_arrays.cpp
@@ -28,6 +28,10 @@ int main()
    vector<int> tableau(3,2);  //Un tableau de 3 entiers valant tous 2
    tableau.push_back(8);
    //On ajoute une 4ème case au tableau qui contient la valeur 8
+   tableau.push_back(7);  //On ajoute une 5ème case qui contient la valeur 7
+   tableau.push_back(14); //Et encore une avec le nombre 14 cette fois
    
+   //Le tableau contient maintenant les nombres : 2 2 2 8 7 14
+
    return 0;
 }
