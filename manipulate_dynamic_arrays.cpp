ecb39dbe19f24f32bd844cf7ad3d75d175d6abf5
WizzOFF <maher_off@proton.me>
2025-03-30 05:34:13 +0200

    manipulate_dynamic_arrays.cpp: view a string as an array > manipulate string

--- manipulate_dynamic_arrays.cpp
+++ manipulate_dynamic_arrays.cpp
@@ -99,5 +99,15 @@ int main()
    string texte("Portez ce whisky au vieux juge blond qui fume.");  //46 caractères
    cout << "Cette phrase contient " << texte.size() << " lettres." << endl;
 
+   string prenom("Albert"); 
+   string nom("Einstein");
+   
+   string total;    //Une chaîne vide
+   total += prenom; //On ajoute le prénom à la chaîne vide
+   total += " ";    //Puis un espace
+   total += nom;    //Et finalement le nom de famille
+
+   cout << "Vous vous appelez " << total << "." << endl;
+   
    return 0;
 }
